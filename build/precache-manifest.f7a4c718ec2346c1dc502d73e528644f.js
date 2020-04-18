a2b264f60782f046508bb49ebefa10830a21f765
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-18 16:04:20 -0400

    uibuild

---
+++ build/precache-manifest.f7a4c718ec2346c1dc502d73e528644f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f69e7d103672d3d7f5b0a83dadfd9f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "434581fc3225e54a1cc4",
+    "url": "/static/css/main.3f868a96.chunk.css"
+  },
+  {
+    "revision": "f5ffb0dc48ad9952bcfd",
+    "url": "/static/js/2.5c68fe71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c68fe71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "434581fc3225e54a1cc4",
+    "url": "/static/js/main.a509572d.chunk.js"
+  },
+  {
+    "revision": "cbe135e57de162cd240f",
+    "url": "/static/js/runtime-main.d0b0a789.js"
+  }
+]);
