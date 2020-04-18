059a0e1f98e0d826722745303e5bde9055d04762
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-18 14:07:23 -0400

    exercise 3.8

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const morgan = require('morgan')
 
+morgan.token('json-body', (req, res) => {
+  return JSON.stringify(req.body)
+})
+
 const app = express()
 
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan('tiny', { skip: (req, res) => req.method === 'POST' }))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json-body',
+      { skip: (req, res) => req.method !== 'POST' }))
 
 let persons = [
 	{   
