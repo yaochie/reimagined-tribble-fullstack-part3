760cbf1422ecb4519a856438bee28f17ca840d6a
Yao Chong <10258156+yaochie@users.noreply.github.com>
2020-04-18 13:26:05 -0400

    exercise 3.3-4

--- index.js
+++ index.js
@@ -26,14 +26,37 @@ let persons = [
 	}   
 ]
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
 app.get('/info', (request, response) => {
 	const numString = `<p>Phonebook has ${persons.length} entries</p>`
 	const timeString = `<p>${new Date()}</p>`
 	response.send(numString + timeString)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const person = persons.find(person => person.id === id)
+
+	if (person) {
+		response.json(person)
+	} else {
+		response.status(404).end()
+	}
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const person = persons.find(person => person.id === id)
+
+	if (person) {
+		persons = persons.filter(person => person.id !== id)
+		response.status(200).end()
+	} else {
+		response.status(404).end()
+	}
 })
 
 const PORT = 3001
